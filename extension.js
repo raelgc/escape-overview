a8be6ed418acdf0e714bd59570ffbfd1e4fac43c
Rael Gugelmin Cunha <rael.gc@gmail.com>
2024-11-29 10:35:42 -0300

    Proper upgrade to Gnome 46

--- extension.js
+++ extension.js
@@ -1,18 +1,14 @@
-const {Clutter, St} = imports.gi;
-const Main = imports.ui.main;
-const SearchController = Main.overview.viewSelector ?
-                                   Main.overview.viewSelector :
-                                   Main.overview._overview.controls._searchController;
-let previousStageKeyPress;
+import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
+import Clutter from 'gi://Clutter';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 
+const SearchController = Main.overview._overview.controls._searchController;
 
-// Based on _onStageKeyPress function from https://gitlab.gnome.org/GNOME/gnome-shell/-/blob/gnome-40/js/ui/searchController.js
-function _onStageKeyPress(actor, event) {
+// Based on _onStageKeyPress function from https://gitlab.gnome.org/GNOME/gnome-shell/-/blob/gnome-46/js/ui/searchController.js
+function _onStageKeyPress(_actor, event) {
   if (Main.modalCount > 1)
     return Clutter.EVENT_PROPAGATE;
-
   let symbol = event.get_key_symbol();
-
   if (symbol === Clutter.KEY_Escape) {
     if (this._searchActive)
       this.reset();
@@ -20,46 +16,22 @@ function _onStageKeyPress(actor, event) {
       Main.overview.hide();
       return Clutter.EVENT_STOP;
   } else if (this._shouldTriggerSearch(symbol)) {
-      this.startSearch(event);
+    this.startSearch(event);
   }
   return Clutter.EVENT_PROPAGATE;
 }
 
-// Based on _onStageKeyPress function from https://github.com/GNOME/gnome-shell/blob/gnome-3-36/js/ui/viewSelector.js
-function _onStageKeyPressOld(actor, event) {
-  if (Main.modalCount > 1)
-    return Clutter.EVENT_PROPAGATE;
-
-  let symbol = event.get_key_symbol();
-
-  if (symbol === Clutter.KEY_Escape) {
-    if (SearchController._searchActive)
-      SearchController.reset();
-    else
-      Main.overview.hide();
-      return Clutter.EVENT_STOP;
-  } else if (this._shouldTriggerSearch(symbol)) {
-      this.startSearch(event);
-  } else if (!this._searchActive && !global.stage.key_focus) {
-    if (symbol === Clutter.KEY_Tab || symbol === Clutter.KEY_Down) {
-      this._activePage.navigate_focus(null, St.DirectionType.TAB_FORWARD, false);
-      return Clutter.EVENT_STOP;
-    } else if (symbol === Clutter.KEY_ISO_Left_Tab) {
-      this._activePage.navigate_focus(null, St.DirectionType.TAB_BACKWARD, false);
-      return Clutter.EVENT_STOP;
-    }
+export default class EscapeOverview extends Extension {
+  constructor(metadata) {
+    super(metadata);
+    this.defaultOnStageKeyPress = SearchController._onStageKeyPress;
   }
-  return Clutter.EVENT_PROPAGATE;
-}
 
-function init() {
-  previousStageKeyPress = SearchController._onStageKeyPress;
-}
+  enable() {
+    SearchController._onStageKeyPress = _onStageKeyPress;
+  }
 
-function enable() {
-  SearchController._onStageKeyPress = Main.overview.viewSelector ? _onStageKeyPressOld : _onStageKeyPress;
-}
-
-function disable() {
-  SearchController._onStageKeyPress = previousStageKeyPress;
+  disable() {
+    SearchController._onStageKeyPress = this.defaultOnStageKeyPress;
+  }
 }
